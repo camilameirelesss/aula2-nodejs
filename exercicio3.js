7fa23f66039e9ce37687e009fe93926cf6374c01
Camila Meireles <camilameireles_ss@yahoo.com.br>
2021-05-21 23:40:07 -0300

    Resolução do exercicio 3

---
+++ exercicio3.js
@@ -0,0 +1,5 @@
+let valorJantar = 400
+let taxaGarcom = valorJantar * 0.1
+let valorTotal = valorJantar + taxaGarcom
+console.log(`taxaGarcom = ${taxaGarcom}`)
+console.log(`valorTotal = ${valorTotal}`)
